3c685144126aa80c0b5898b2cb157eb62874e11d
siemasusel <jantomx@gmail.com>
2019-05-20 23:06:37 +0200

    stats and eq
    Add statistics and weapons for player

---
+++ Assets/Scripts/InventoryControler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryControler : MonoBehaviour
+{
+    public Item sword, axe, dagger;
+    public PlayerWeaponControler playerWeaponControler;
+
+    void Start()
+    {
+        playerWeaponControler = GetComponent<PlayerWeaponControler>();
+        List<BaseStat> swordStats = new List<BaseStat>();
+        List<BaseStat> axeStats = new List<BaseStat>();
+        List<BaseStat> daggerStats = new List<BaseStat>();
+        swordStats.Add(new BaseStat(6, "Power", "Your power level."));
+        axeStats.Add(new BaseStat(14, "Power", "Your power level."));
+        daggerStats.Add(new BaseStat(4, "Power", "Your power level."));
+        sword = new Item(swordStats, "sword");
+        axe = new Item(axeStats, "axe");
+        dagger = new Item(daggerStats, "dagger");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            playerWeaponControler.EquipWeapon(sword);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            playerWeaponControler.EquipWeapon(axe);
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            playerWeaponControler.EquipWeapon(dagger);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerWeaponControler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponControler : MonoBehaviour
+{
+    public GameObject playerHand;
+    public GameObject EquippedWeapon { get; set; }
+
+    IWeapon equippedWeapon;
+    CharacterStats characterStats;
+
+    void Start()
+    {
+        characterStats = GetComponent<CharacterStats>();
+        
+    }
+
+    public void EquipWeapon(Item ItemToEquip)
+    {
+        if (EquippedWeapon != null)
+        {
+            characterStats.RemoveStatBonus(EquippedWeapon.GetComponent<IWeapon>().Stats);
+            Destroy(playerHand.transform.GetChild(0).gameObject);
+        }
+        EquippedWeapon = (GameObject)Instantiate(Resources.Load<GameObject>("Weapons/" + ItemToEquip.ObjectSlug), playerHand.transform.position, playerHand.transform.rotation);
+        equippedWeapon = EquippedWeapon.GetComponent<IWeapon>();
+        equippedWeapon.Stats = ItemToEquip.Stats;
+        EquippedWeapon.transform.SetParent(playerHand.transform);
+        characterStats.AddStatBonus(ItemToEquip.Stats);
+
+        Debug.Log(characterStats.stats[0].StatName + ": " + characterStats.stats[0].GetCalculatedStatValue() + " | " + characterStats.stats[1].StatName + ": " + characterStats.stats[1].GetCalculatedStatValue());
+
+    }
+
+    public void PerformWeaponAttack()
+    {
+        equippedWeapon.PerformAttack();
+    }
+}
